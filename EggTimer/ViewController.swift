b59f154de991d2f86aa7c53ff558690824709030
stanmill <stanley.pena29@gmail.com>
2020-05-20 20:11:34 -0700

    egg timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,58 @@
 //
 
 import UIKit
+import AVFoundation // Sound module
 
 class ViewController: UIViewController {
     
+    // Dictionary (uses key value pairs)
+    let eggTimes: [String : Int] = ["Soft": 3, "Medium": 420, "Hard": 720]
+    var secondsPassed = 0
+    var totalTime = 0
+    var timer = Timer()
+    // Create an instance of the audio object
+    var player = AVAudioPlayer()
 
+    @IBOutlet weak var doneTitle: UILabel!
+    @IBOutlet weak var eggProgress: UIProgressView!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate() // stops the timer
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        doneTitle.text = hardness
+        
+        // Resets things back to zero
+        eggProgress.progress = 0.0
+        secondsPassed = 0
+                
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.secondsPassed < self.totalTime {
+                
+                self.secondsPassed += 1
+                let percentageProgress: Float = Float(self.secondsPassed) / Float(self.totalTime)
+                self.eggProgress.progress = Float(percentageProgress)
+                print(percentageProgress)
+            } else {
+                // Stops the timer
+                Timer.invalidate()
+                self.doneTitle.text = "Done"
+                self.playSound()
 
+            }
+        }
+        
+    }
+    
+    func playSound() {
+        // Gets a refrence to the audio file
+        // Passes user input
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+
+    }
+    
 }
